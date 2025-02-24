694f9cd4927fd266c91c409eab6d6b6b66709e3b
siammahamud <siammahamud710@gmail.com>
2025-02-24 23:43:35 +0600

    problem 6 solve by method 2 math.max

--- Simple/problem_6.js
+++ Simple/problem_6.js
@@ -1,7 +1,13 @@
+///////find max number of an array ///////////
+
 const FindMax = (arr) => {
   arr = arr.reduce((acc, cur) => (acc > cur ? acc : cur));
   return arr;
 };
 
+const findMax = (arr) => {
+  return Math.max(...arr); ///// spread operator converts the array into numbers //////
+};
+
 const numbers = [2, 4, 5, 9, 4, 7, 3];
-console.log(FindMax(numbers));
+console.log(findMax(numbers));
