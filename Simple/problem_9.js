c3d1c9430adf6b3a2f1097b0d6d80de07551d022
siammahamud <siammahamud710@gmail.com>
2025-02-26 20:04:33 +0600

    problem 9 is solved by new Every method

--- Simple/problem_9.js
+++ Simple/problem_9.js
@@ -1,16 +1,21 @@
 const arr1 = [1, 2, 3, 4, 5];
-const arr2 = [1, 2, 3, 4, 5];
+const arr2 = [1, 2, 5, 4, 5];
+
+// const AreArrEqual = (arr1, arr2) => {
+//   if (arr1.length !== arr2.length) return false;
+//   else {
+//     for (let i = 0; i < arr1.length; i++) {
+//       if (arr1[i] !== arr2[i]) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// };
 
 const AreArrEqual = (arr1, arr2) => {
   if (arr1.length !== arr2.length) return false;
-  else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
+  return arr1.every((curr, index) => curr === arr2[index]);
 };
 
 console.log(AreArrEqual(arr1, arr2));
