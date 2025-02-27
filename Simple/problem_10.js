5cb19696c0871cc1211c443e2cc41f513f5770ed
siammahamud <siammahamud710@gmail.com>
2025-02-27 23:03:58 +0600

    problem 10 is solved by method2

--- Simple/problem_10.js
+++ Simple/problem_10.js
@@ -1,11 +1,16 @@
 ///Calculate the sum of digits of a number////
+// const sumOfDigits = (num) => {
+//   num = num.toString().split("");
+//   const sum = num.reduce((acc, curr) => parseInt(curr) + parseInt(acc), 0);
+//   return sum;
+// };
+
+
 const sumOfDigits = (num) => {
-  num = num.toString().split("");
-  const sum = num.reduce((acc, curr) => parseInt(curr) + parseInt(acc), 0);
-  return sum;
-};
+   const arr = Array.from(String(num), Number);
+   return arr.reduce((acc, curr) => acc + curr, 0);
+}
 
 
 
-
-console.log(sumOfDigits(11224));
+console.log(sumOfDigits(112249));
