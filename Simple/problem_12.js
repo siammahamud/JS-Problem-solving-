b0918f728e85b5d1fa705a0265ed82fec44a9116
siammahamud <siammahamud710@gmail.com>
2025-03-01 22:22:40 +0600

    problem 12 solved by a new set method

--- Simple/problem_12.js
+++ Simple/problem_12.js
@@ -1,16 +1,22 @@
+// const removeDuplicate = (arr) => {
+//   const arrCount = {};
+//   let ans = [];
+//   for (let num of arr) {
+//     arrCount[num] = (arrCount[num] || 0) + 1;
+//   }
+//   console.log(arrCount);
+//   for (let num of arr) {
+//     if (arrCount[num] === 1) {
+//       ans.push(num);
+//     }
+//   }
+//   return ans;
+// };
+
+// Method 2 new set method ////////
 const removeDuplicate = (arr) => {
-  const arrCount = {};
-  let ans = [];
-  for (let num of arr) {
-    arrCount[num] = (arrCount[num] || 0) + 1;
-  }
-  console.log(arrCount);
-  for (let num of arr) {
-    if (arrCount[num] === 1) {
-      ans.push(num);
-    }
-  }
-  return ans;
+  let newArr = [...new Set(arr)];
+  return newArr;
 };
 
 console.log(removeDuplicate([1, 2, 2, 4, 5, 6, 6]));
