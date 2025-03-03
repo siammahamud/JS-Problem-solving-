7e918017c90d72a0eab79a7b6cf9d40b8642b32a
siammahamud <siammahamud710@gmail.com>
2025-03-03 09:24:51 +0600

    problem 14 solved by bitwise operator

--- Simple/problem_14.js
+++ Simple/problem_14.js
@@ -5,4 +5,14 @@ const findPowerOfTwo = (num) => {
 
   return false;
 };
-console.log(findPowerOfTwo(-32));
+// console.log(findPowerOfTwo(-32));
+
+function isPowerOfTwo(n) {
+  return n > 0 && (n & (n - 1)) === 0;
+}
+
+console.log(isPowerOfTwo(8)); // true (8 = 1000)
+console.log(isPowerOfTwo(16)); // true (16 = 10000)
+console.log(isPowerOfTwo(10)); // false (10 = 1010)
+console.log(isPowerOfTwo(0)); // false
+console.log(isPowerOfTwo(1)); // true (1 = 0001)
