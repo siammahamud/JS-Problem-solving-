d2eda357f7bf4b6b615eda2e115a845913dbdf00
siammahamud <siammahamud710@gmail.com>
2025-03-04 22:24:54 +0600

    finding minimum number in a array with Math.min()  method

--- Simple/problem_15.js
+++ Simple/problem_15.js
@@ -5,8 +5,9 @@
 // console.log(sumOfSquare([1, 2, 3]));
 
 const findMinimum = (arr) => {
-  arr = arr.sort((a, b) => a - b);
-  return arr[0];
+//   arr = arr.sort((a, b) => a - b);
+//   return arr[0];
+   return Math.min(...arr);  /// by spreading array , converting the array into numbers 
 };
 
-console.log(findMinimum([5, 2, 3]));
+console.log(findMinimum([5, 2, 3, 4, 5, 1]));
