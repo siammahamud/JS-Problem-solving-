14c17a78b459b3e209db4eeb46493bf794f6f005
siammahamud <siammahamud710@gmail.com>
2025-03-04 22:22:03 +0600

    finding minimum number in a array with sort method

--- Simple/problem_15.js
+++ Simple/problem_15.js
@@ -1,5 +1,12 @@
-function sumOfSquare(arr) {
-  return arr.reduce((acc, curr) => acc += ( curr * curr),0);
-}
+// function sumOfSquare(arr) {
+//   return arr.reduce((acc, curr) => (acc += curr * curr), 0);
+// }
 
-console.log(sumOfSquare([1, 2, 3]));
+// console.log(sumOfSquare([1, 2, 3]));
+
+const findMinimum = (arr) => {
+  arr = arr.sort((a, b) => a - b);
+  return arr[0];
+};
+
+console.log(findMinimum([5, 2, 3]));
