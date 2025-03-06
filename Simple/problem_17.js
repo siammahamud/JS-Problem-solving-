477200e722125b264b8e31ddf8c6ded69152d0d7
siammahamud <siammahamud710@gmail.com>
2025-03-06 23:57:37 +0600

    problem 17 is solved

--- Simple/problem_17.js
+++ Simple/problem_17.js
@@ -1,11 +1,12 @@
+///write a function to check if a given string starts with a specific string//
 const findSameStr = (arr) => {
-return  arr.map((str) => {
-    if (str.includes(arr[0])) {
-      return true;
-    } else {
-      return false;
-    }
-  });
+  let givenStr = arr[0];
+  givenStr = givenStr.split(" ");
+  if (givenStr[0].toLowerCase() === arr[1].toLowerCase()) return true;
+  else {
+    return false;
+  }
+  console.log(givenStr);
 };
 
-console.log(findSameStr(["hello", "hello world"]));
+console.log(findSameStr(["hello world", "hellO"]));
