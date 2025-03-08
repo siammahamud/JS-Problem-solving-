7a3ebb9f7a5bae935bf4918822e60ad87c6756e5
siammahamud <siammahamud710@gmail.com>
2025-03-08 21:51:12 +0600

    problem 17 solved

--- Simple/problem_17.js
+++ Simple/problem_17.js
@@ -1,18 +1,18 @@
-///write a function to check if a given string starts with a specific string//
-// const findSameStr = (arr) => {
-//   let givenStr = arr[0];
-//   givenStr = givenStr.split(" ");
-//   if (givenStr[0].toLowerCase() === arr[1].toLowerCase()) return true;
-//   else {
-//     return false;
-//   }
-//   console.log(givenStr);
-// };
-
-// console.log(findSameStr(["hello world", "hellO"]));
-
-const startWith = (str, subStr) => {
-  return str.toLowerCase().startsWith(subStr.toLowerCase());
+/write a function to check if a given string starts with a specific string//
+const findSameStr = (arr) => {
+  let givenStr = arr[0];
+  givenStr = givenStr.split(" ");
+  if (givenStr[0].toLowerCase() === arr[1].toLowerCase()) return true;
+  else {
+    return false;
+  }
+  console.log(givenStr);
 };
 
-console.log(startWith("hello world", "hellO"));
+console.log(findSameStr(["hello world", "hellO"]));
+
+// const startWith = (str, subStr) => {
+//   return str.toLowerCase().startsWith(subStr.toLowerCase());
+// };
+
+// console.log(startWith("hello world", "hellO"));
--- Simple/problem_18.js
+++ Simple/problem_18.js
@@ -3,7 +3,7 @@ const findMedian = (arr) => {
   let middle = Math.floor(arr.length/ 2 );
   const newArr = arr.sort((a, b) => a - b);
   if (arr.length % 2 === 0) {
-    const result = (arr[middle] + arr[middle + 1]) / 2;
+    const result = (arr[middle] + arr[middle - 1]) / 2;
     return result;
   } else if(arr.length %2 !== 0) {
     return arr[middle];
